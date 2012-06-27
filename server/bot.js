b04d1ed486537ea29e3460750c68e145cc22b909
Daniel Kuehn <daniel@kuehn.se>
2012-06-27 23:34:22 +0200

    Alternate method of "timeout"

--- server/bot.js
+++ server/bot.js
@@ -5,30 +5,31 @@ var handle = query.observe({
     if (!message.is_bot) {
 	var n = Math.floor(Math.random()*5);
 	var message = null;
-//	setTimeout(function() {
-	    switch (n) {
-	    case 0:
-		message = 'Tihi, your shirt is untucked. *giggles*';
-		break;
-	    case 1:
-		message = 'Anata wa baka desu ka.';
-		break;
-	    case 2:
-		message = 'Kore wa baka desu ka.';
-		break;
-	    case 3:
-		message = 'Trololololololololololololololol';
-		break;
-	    case 4:
-		message = 'This is a easter egg, that is in summer';
-		break;
-	    default:
-		message = 'Hi! You are so cute!';
-		break;
-	    }
+	var date = new Date();
+	//	setTimeout(function() {
+	switch (n) {
+	case 0:
+	    message = 'Tihi, your shirt is untucked. *giggles*';
+	    break;
+	case 1:
+	    message = 'Anata wa baka desu ka.';
+	    break;
+	case 2:
+	    message = 'Kore wa baka desu ka.';
+	    break;
+	case 3:
+	    message = 'Trololololololololololololololol';
+	    break;
+	case 4:
+	    message = 'This is a easter egg, that is in summer';
+	    break;
+	default:
+	    message = 'Hi! You are so cute!';
+	    break;
+	}
 
-	    Messages.insert({ author: 'Ichimitsu', timestamp: new Date(), text: message, is_bot: true }) ;
-//	}, n*1000);
+	Messages.insert({ author: 'Ichimitsu', timestamp: date.setTime(date.getTime() + n*1000), text: message, is_bot: true }) ;
+	//	}, n*1000);
     }
   }
 });
