f73ecf90195ad6c9f4e432bc1de63a47b2f1d8d3
Daniel Kuehn <daniel@kuehn.se>
2012-06-27 22:26:34 +0200

    Made 4 different things the bot can say

--- server/bot.js
+++ server/bot.js
@@ -3,7 +3,20 @@ var query = Messages.find({});
 var handle = query.observe({
   added: function(message) {
     if (!message.is_bot) {
-      Messages.insert({ author: 'Ichimitsu', timestamp: new Date(), text: 'hi, you are so cute!', is_bot: true }) 
+	var n = Math.floor(Math.random()*5);
+	var message = null;
+	switch (n) {
+	case 1:
+	    message = 'Tihi, your shirt is untucked. *giggles*';
+	case 2:
+	    message = 'Anata wa baka desu ka.';
+	case 3:
+	    message = 'Kore wa baka desu ka.';
+	default:
+	    message = 'Hi! You are so cute!';
+	}
+
+	Messages.insert({ author: 'Ichimitsu', timestamp: new Date(), text: message, is_bot: true }) ;
     }
   }
 });
